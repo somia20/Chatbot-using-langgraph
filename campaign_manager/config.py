f405b784c7ffbfafdf90bb6145715e1ccad9a436
Somia Kumari <unibicsomia20@gmail.com>
2025-03-20 12:23:29 +0530

    campaign_manager

--- campaign_manager/config.py
+++ campaign_manager/config.py
@@ -2,6 +2,6 @@ import os
 os.environ["GROQ_API_KEY"] = ""
 from langchain_groq import ChatGroq
 llm = ChatGroq(
-    model_name="llama-3.3-70b-versatile",
+    model_name="lama-3.3-70b-versatile",
     temperature=0
 )
