1048ae2c59b7acf1a0f9921c2eb84e6adea99875
Somia Kumari <unibicsomia20@gmail.com>
2025-03-20 12:13:54 +0530

    added lann2

--- lann 2/lann/config.py
+++ lann 2/lann/config.py
@@ -1,5 +1,5 @@
 import os
-os.environ["GROQ_API_KEY"] = "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"
+os.environ["GROQ_API_KEY"] = ""
 from langchain_groq import ChatGroq
 llm = ChatGroq(
     model_name="llama-3.3-70b-versatile",
